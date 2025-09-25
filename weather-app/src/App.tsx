7b046d93b9f4124756d315747ef1ab168b6411fe
Pete Hodgson <git@thepete.net>
2025-09-25 10:15:16 -0700

    wire up React feature flags

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -3,23 +3,26 @@ import { AuthProvider } from "@/providers/AuthContext";
 import { AuthGuard } from "@/components/AuthGuard";
 import { LoginPage } from "@/components/LoginPage";
 import { WeatherApp } from "@/components/WeatherApp";
+import { FeatureFlagsProvider } from "./providers/FeatureFlags";
 
 export default function App() {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/"
-            element={
-              <AuthGuard>
-                <WeatherApp />
-              </AuthGuard>
-            }
-          />
-        </Routes>
-      </Router>
+      <FeatureFlagsProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/"
+              element={
+                <AuthGuard>
+                  <WeatherApp />
+                </AuthGuard>
+              }
+            />
+          </Routes>
+        </Router>
+      </FeatureFlagsProvider>
     </AuthProvider>
   );
 }
